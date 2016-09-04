420999b48113524333e8e1a21c02b8ca1812a8a9
Sun8877777 <an-cheremisin@yandex.ru>
2016-09-05 02:26:13 +0300

    final_v2

--- js/nerds.js
+++ js/nerds.js
@@ -4,17 +4,10 @@
   var form = popup.querySelector("form");
   var login = popup.querySelector("[name=modal-user-name]");
   var email = popup.querySelector("[name=modal-email]")
-  /*var storage = localStorage.getItem("login");*/
   
   link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.add("modal-content-show");
-    if (storage) {
-      login.value = storage;
-      email.focus();
-    } else {
-      login.focus();
-      }
   });
   
   close.addEventListener("click", function(event) {
@@ -22,6 +15,7 @@
     popup.classList.remove("modal-content-show");
     popup.classList.remove("modal-error");
   });
+  
   form.addEventListener("submit", function(event) {
     if (!login.value || !email.value)
       {
@@ -32,6 +26,7 @@
             localStorage.setItem("login", login.value);
         }
   });
+  
   window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27) {
       if (popup.classList.contains("modal-content-show")) {
